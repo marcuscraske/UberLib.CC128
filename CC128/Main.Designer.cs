7fab2e9ab137291033281e266318ae53378f2f2f
Limpy Gnome <limpygnome@gmail.com>
2012-07-08 06:35:04 +0100

    First commit

---
+++ CC128/Main.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace CC128_Test
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttClear = new System.Windows.Forms.Button();
+            this.buttExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(512, 258);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttClear
+            // 
+            this.buttClear.Location = new System.Drawing.Point(449, 276);
+            this.buttClear.Name = "buttClear";
+            this.buttClear.Size = new System.Drawing.Size(75, 23);
+            this.buttClear.TabIndex = 1;
+            this.buttClear.Text = "Clear";
+            this.buttClear.UseVisualStyleBackColor = true;
+            this.buttClear.Click += new System.EventHandler(this.buttClear_Click);
+            // 
+            // buttExit
+            // 
+            this.buttExit.Location = new System.Drawing.Point(12, 276);
+            this.buttExit.Name = "buttExit";
+            this.buttExit.Size = new System.Drawing.Size(75, 23);
+            this.buttExit.TabIndex = 2;
+            this.buttExit.Text = "Exit";
+            this.buttExit.UseVisualStyleBackColor = true;
+            this.buttExit.Click += new System.EventHandler(this.buttExit_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(536, 304);
+            this.Controls.Add(this.buttExit);
+            this.Controls.Add(this.buttClear);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.Text = "CC128 - Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttClear;
+        private System.Windows.Forms.Button buttExit;
+    }
+}
+
---
+++ CC128/Main.cs
@@ -0,0 +1,68 @@
+﻿﻿/*
+ * UBERMEAT FOSS
+ * ****************************************************************************************
+ * License:                 Creative Commons Attribution-ShareAlike 3.0 unported
+ *                          http://creativecommons.org/licenses/by-sa/3.0/
+ * 
+ * Project:                 UberLib.CC128
+ * File:                    /Main.cs
+ * Author(s):               limpygnome						limpygnome@gmail.com
+ * To-do/bugs:              none
+ * 
+ * Responsible for demonstrating and testing the library with the CC128.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Xml;
+using UberLib.CC128;
+
+namespace CC128_Test
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EnergyMonitor mon = new EnergyMonitor();
+            mon.eventNewSensorData += new EnergyMonitor._eventNewSensorData(mon_eventNewSensorData);
+            mon.eventNewSensorDataMalformed += new EnergyMonitor._eventNewSensorDataMalformed(mon_eventNewSensorDataMalformed);
+            mon.eventStateChange += new EnergyMonitor._eventStateChange(mon_eventStateChange);
+            mon.start();
+        }
+        void mon_eventStateChange(EnergyMonitor.State newState)
+        {
+            textbox("New state: " + newState.ToString() + "\r\n");
+        }
+        void mon_eventNewSensorDataMalformed(string data, Exception ex)
+        {
+            textbox("Malformed data - " + ex.Message + "\r\n");
+        }
+        void mon_eventNewSensorData(EnergyReading reading)
+        {
+            textbox(reading[0] + " watts - " + reading.CurrentTime + " monitor time - " + reading.Temperature + " celsius - " + reading.Software + "\r\n");
+        }
+        void textbox(string data)
+        {
+            textBox1.Invoke((MethodInvoker)delegate()
+            {
+                textBox1.Text += DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss: ") + data;
+                textBox1.SelectionStart = textBox1.TextLength;
+                textBox1.ScrollToCaret();
+            });
+        }
+        private void buttClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+        private void buttExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
